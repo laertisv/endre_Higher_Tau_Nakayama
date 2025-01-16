c20e0f2baeedd922088289bf5d700c873c78139b
laertisv <laertis.vaso@gmail.com>
2025-01-16 15:50:54 +0100

    Update torsion.js

--- js/torsion/torsion.js
+++ js/torsion/torsion.js
@@ -1,1 +1,17 @@
+function computeTorsionClass(){
+  chosenRigid = [];
+  chosenSupport = [];
+  possibleMutations = [];
+  drawARquiver(d,p,l,'displayDivForARQuiver');
+}
 
+
+function obtainTorsionClass(){
+  let btnTorsion_old = document.getElementById('torsionButton');
+  let btnTorsion = btnMutate_old.cloneNode(true);
+  btnToesion_old.parentNode.replaceChild(btnTorsion,btnTorsion_old);
+  btnTorsion.disabled = false;
+  btnTorsion.addEventListener('click',function(){
+    computeTorsionClass();
+  });
+}
